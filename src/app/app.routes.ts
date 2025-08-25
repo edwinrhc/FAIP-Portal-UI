913adc0be5c63da8ff36e18fe6813dee48fa5dea
Edwin HC <edwinrhc@gmail.com>
2025-08-25 18:59:39 -0500

    "feat(auth): Implement AuthGuard and protect /admin route"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./auth/pages/login/login.component";
 import {RegisterComponent} from "./auth/pages/register/register.component";
+import {AdminPanelComponent} from "./pages/admin-panel/admin-panel.component";
+import {authGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
 
   {path:'', redirectTo:'auth/login', pathMatch:'full'},
   {path:'auth/login', component: LoginComponent },
   {path:'auth/register', component: RegisterComponent },
+  {path:'admin', component: AdminPanelComponent, canActivate: [authGuard] },
 
 ];
